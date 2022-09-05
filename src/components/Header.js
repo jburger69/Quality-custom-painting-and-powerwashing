76845142f3f29188c0324013475872275f306e0f
Joshua Burger <joshberger6969@outlook.com>
2022-09-05 15:19:16 -0400

    Comment out image for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import heroImage from '../images/painters photo.webp'
+import estimateImage from '../images/free estimates-fococlipping-standard.png'
 
 export default function Header() {
   return (
@@ -8,6 +9,7 @@ export default function Header() {
         <h1 className='company_name'>Quality Custom Painting and PowerWashing</h1>
         <h1 className='company_number'>570-269-7523</h1>
         <img className='company_image' src={heroImage} alt=""/>
+        {/* <img className='company_estimate_image' src={estimateImage} alt=""/> */}
     </div>
   )
 }
