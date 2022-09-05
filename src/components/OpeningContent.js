3ed53855507d0edbdac5ccba7c48897378df7da0
Joshua Burger <joshberger6969@outlook.com>
2022-09-05 15:31:11 -0400

    Add openingcontent function

---
+++ src/components/OpeningContent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './OpeningContent.css'
+
+export default function OpeningContent() {
+  return (
+    <div className='opening_content'>
+        <div className='opening_images'>
+            {/* images */}
+            <h1>Images</h1>
+        </div>
+        <div className='opening_text'>
+            <h1>Services We Offer</h1>
+            <h3>Window Washing</h3>
+            <h3>Power Washing</h3>
+            <h3>Painting</h3>
+            <h3>Interior / Exterior</h3>
+            <h3>Commercial / Residental</h3>
+        </div>
+    </div>
+  )
+}
