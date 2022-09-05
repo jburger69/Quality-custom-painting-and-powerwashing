3440e4b9244f471fe3e553bf68d7f0f75622169f
Joshua Burger <joshberger6969@outlook.com>
2022-09-05 15:09:10 -0400

    Add image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Header.css'
+import heroImage from '../images/painters photo.webp'
 
 export default function Header() {
   return (
     <div className='Header'>
         <h1 className='company_name'>Quality Custom Painting and PowerWashing</h1>
         <h1 className='company_number'>570-269-7523</h1>
+        <img className='company_image' src={heroImage} alt=""/>
     </div>
   )
 }
