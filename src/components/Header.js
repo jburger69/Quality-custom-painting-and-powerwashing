5743f165e5b1955e3e9acca2788cc6fd605c7dc4
Joshua Burger <joshberger6969@outlook.com>
2022-09-05 14:54:01 -0400

    Header component to display name/number

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='Header'>
+        <h1 className='company_name'>Quality Custom Painting and PowerWashing</h1>
+        <h1 className='company_number'>570-269-7523</h1>
+    </div>
+  )
+}
