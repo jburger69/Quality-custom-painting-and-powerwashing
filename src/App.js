33f349ad0112e7730c56ea8bea37c940da766f60
Joshua Burger <joshberger6969@outlook.com>
2022-09-05 15:30:38 -0400

    Import opening content component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 //Components
 import Header from './components/Header';
+import OpeningContent from './components/OpeningContent';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <OpeningContent />
     </div>
   );
 }
