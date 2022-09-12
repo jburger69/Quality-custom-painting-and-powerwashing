85baef2e73445b83aac152b26a40d48a5dfa291a
Joshua Burger <joshberger6969@outlook.com>
2022-09-12 14:13:36 -0400

    Add new ClientGallery component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 //Components
 import Header from './components/Header';
 import OpeningContent from './components/OpeningContent';
+import ClientGallery from './components/ClientGallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <OpeningContent />
+      <ClientGallery />
     </div>
   );
 }
