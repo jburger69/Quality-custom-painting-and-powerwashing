80a3c13da9e2a8fc3703dd5e5f45c10fb1c688ff
Joshua Burger <joshberger6969@outlook.com>
2022-09-14 17:03:08 -0400

    Change section to ul with list items

--- src/components/OpeningContent.js
+++ src/components/OpeningContent.js
@@ -10,20 +10,22 @@ export default function OpeningContent() {
             <h1>Images</h1>
         </div>
         <div className='opening_text'>
-            <h1>Services We Offer</h1>
-            <h3>Window Washing</h3>
-            <h3>Power Washing</h3>
-            <h3>Painting</h3>
-            <h3>Interior / Exterior</h3>
-            <h3>Commercial / Residental</h3>
+            <ul>Services We Offer
+            <li>Window Washing</li>
+            <li>Power Washing</li>
+            <li>Painting</li>
+            <li>Interior / Exterior</li>
+            <li>Commercial / Residental</li>
+            </ul>
         </div>
     </div>
     <div className='opening_content_two'>
         <div className='opening_text_two'>
-            <h1>Why Us?</h1>
-            <h3>Fully Insured Company</h3>
-            <h3>Over 25 years of painting experience</h3>
-            <h3>We are known for our quality work and competitive pricing.</h3>
+            <ul>Why Us?
+            <li>Fully Insured Company</li>
+            <li>Over 25 years of painting experience</li>
+            <li>We are known for our quality work and competitive pricing.</li>
+            </ul>
         </div>
         <div className='opening_images_two'>
           <h1>Images</h1>
