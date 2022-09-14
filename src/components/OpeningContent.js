1ec1dacb1fe368e426eecab1c584d27d3f357ffe
Joshua Burger <joshberger6969@outlook.com>
2022-09-14 17:06:34 -0400

    Change indents in ul

--- src/components/OpeningContent.js
+++ src/components/OpeningContent.js
@@ -11,20 +11,20 @@ export default function OpeningContent() {
         </div>
         <div className='opening_text'>
             <ul>Services We Offer
-            <li>Window Washing</li>
-            <li>Power Washing</li>
-            <li>Painting</li>
-            <li>Interior / Exterior</li>
-            <li>Commercial / Residental</li>
+              <li>Window Washing</li>
+              <li>Power Washing</li>
+              <li>Painting</li>
+              <li>Interior / Exterior</li>
+              <li>Commercial / Residental</li>
             </ul>
         </div>
     </div>
     <div className='opening_content_two'>
         <div className='opening_text_two'>
             <ul>Why Us?
-            <li>Fully Insured Company</li>
-            <li>Over 25 years of painting experience</li>
-            <li>We are known for our quality work and competitive pricing.</li>
+              <li>Fully Insured Company</li>
+              <li>Over 25 years of painting experience</li>
+              <li>We are known for our quality work and competitive pricing.</li>
             </ul>
         </div>
         <div className='opening_images_two'>
