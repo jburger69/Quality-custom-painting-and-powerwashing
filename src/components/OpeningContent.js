52d130198afd6fc66f77b4a07c6b70cecb9d3779
Joshua Burger <joshberger6969@outlook.com>
2022-11-11 13:10:19 -0500

    Import images, add classNames to style

--- src/components/OpeningContent.js
+++ src/components/OpeningContent.js
@@ -1,34 +1,41 @@
 import React from 'react'
 import './OpeningContent.css'
 
+import image8 from '../images/Image_8.jpeg';
+import image17 from '../images/Image_17.jpeg';
+
 export default function OpeningContent() {
   return (
     <>
     <div className='opening_content'>
         <div className='opening_images'>
-            {/* images */}
-            <h1>Images</h1>
+            <div className='under_image'>
+              <img className='image_one' src={image8} alt=".." width={500} height={400}/>
+            </div>
         </div>
         <div className='opening_text'>
-            <ul>Services We Offer
-              <li>Window Washing</li>
-              <li>Power Washing</li>
-              <li>Painting</li>
-              <li>Interior / Exterior</li>
-              <li>Commercial / Residental</li>
+            <ul className='unordered_list_one'><h1 className='ul_title'>Services We Offer</h1>
+              <li className='list_item'>Window Washing</li>
+              <li className='list_item'>Power Washing</li>
+              <li className='list_item'>Painting</li>
+              <li className='list_item'>Interior / Exterior</li>
+              <li className='list_item'>Commercial / Residental</li>
             </ul>
         </div>
     </div>
     <div className='opening_content_two'>
         <div className='opening_text_two'>
-            <ul>Why Us?
-              <li>Fully Insured Company</li>
-              <li>Over 25 years of painting experience</li>
-              <li>We are known for our quality work and competitive pricing.</li>
+            <ul className='unordered_list_one'><h1 className='ul_title'>Why Us?</h1>
+              <li className='list_item'>Fully Insured Company</li>
+              <li className='list_item'>Over 25 years of painting experience</li>
+              <li className='list_item'>We are known for our quality work and competitive pricing.</li>
+              <li className='list_item'>Our customer's always return for more work.</li>
             </ul>
         </div>
         <div className='opening_images_two'>
-          <h1>Images</h1>
+            <div className='over_image_two'>
+              <img className='image_two' src={image17} alt=".." width={400} height={400}/>
+          </div>
         </div>
       </div>
       <hr className='opening_content_line'></hr>
